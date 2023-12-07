fb6dbd6811b92f72c2205cad63a1ffddb1f9506d
Kamsnyc <kamel.singh1999@gmail.com>
2023-12-06 20:26:48 -0500

    added my own badges

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -25,20 +25,28 @@ export default function Menu() {
   const [activeCategory, setActiveCategory] = useState(menuCategories[0]);
 
   return (
-    <div className="flexlg:flex-row">
-      {/* FILTER */}
-      <div className="lg:w-1/10 overflow-y-auto border-r p-4 md:mt-[32px] flex whitespace-nowrap">
-        {menuCategories.map((category) => (
-          <div
-            key={category}
-            onClick={() => setActiveCategory(category)}
-            className={`cursor-pointer px-4 py-1 rounded-[10px] ${
-              category === activeCategory ? 'red text-white' : ''
-            }`}
-          >
-            {category}
+    <div className="lg:flex-row">
+        <div className=" flex flex-col items-center justify-center">
+        <h1 className=" text-center mt-[24px] md:mt-[30px] text-[48px] md:text-[64px] font-bold leading-[52px] md:leading-[72px]">
+            Our Menu
+          </h1>
+          <p className='max-w-[600px] text-center'>Lacus lobortis nullam nam consectetur fermentum mattis pellentesque id nulla. Risus convallis iaculis risus ac aliquam sit ultricies. Adipiscing adipiscing pellentesque tincidunt vitae. Aliquam dolor egestas nam congue elit dolor.</p>
           </div>
-        ))}
+      
+      {/* FILTER */}
+      <div className="lg:w-1/10 overflow-y-auto border-r p-4 mt-[32px] flex gap-x-2 whitespace-nowrap">
+      {menuCategories.map((menuCategory) => (
+  <div
+    key={menuCategory}
+    onClick={() => setActiveCategory(menuCategory)}
+    className={`cursor-pointer px-4 py-1 rounded-[10px] ${
+      menuCategory === activeCategory ? 'red text-white' : ''
+    }`}
+  >
+    {menuCategory}
+  </div>
+))}
+This way, you
       </div>
 
       {/* MENU ITEMS */}
