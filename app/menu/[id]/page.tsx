b591083351a0a86f6a35cf35c4d54fee1e327215
Kamsnyc <kamel.singh1999@gmail.com>
2023-12-11 14:19:17 -0500

    added order from lalas pizzeria menu + payment

--- app/menu/[id]/page.tsx
+++ app/menu/[id]/page.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
-function page({params}:any) {
-    const {id} = params
-  return (
-    <div>
-      {id}
-    </div>
-  )
-}
+interface PageProps {
+    params: {
+      id: string;
+    };
+  }
+  
+  function Page({ params }: PageProps) {
+    const { id } = params;
+    return (
+      <div>
+        {id}
+      </div>
+    );
+  }
 
-export default page
+export default Page
